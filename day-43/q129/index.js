2408666d116577b473765a73a8e415c043c20767
FarahSyed <farahsyed44@gmail.com>
2024-04-13 17:29:23 +0500

    day 43 challenge complete

--- day-43/q129/index.js
+++ day-43/q129/index.js
@@ -2,7 +2,7 @@
 var _this = this;
 // The regular function's this depends on how it is invoked and whether you're in strict mode or not
 function foo() {
-    console.log(this); // undefined in strict mode otherwise, global object in node.js environment
+    console.log(this); // undefined in strict mode, otherwise global object in node.js environment - because of this substitution
 }
 foo();
 // The arrow function's this value is lexically captured from the parent scope at the time of it's definition regardless of strict and non-strict mode
