9478b5c36090db1caf61d3fbf512bfa266b16290
FarahSyed <farahsyed44@gmail.com>
2024-05-14 08:00:01 +0500

    day 74 challenge complete

---
+++ day-74/dist/debugging.js
@@ -0,0 +1,13 @@
+"use strict";
+// --------------------------- Debugging ---------------------------
+let age = 22;
+if (age > 20) {
+    console.log("Pass");
+}
+else if (age < 20) {
+    console.log("Return");
+}
+else {
+    console.log("Reboot");
+}
+//# sourceMappingURL=debugging.js.map
---
+++ day-74/src/debugging.ts
@@ -0,0 +1,11 @@
+// --------------------------- Debugging ---------------------------
+
+let age: number = 22;
+
+if (age > 20) {
+    console.log("Pass");
+} else if (age < 20) {
+    console.log("Return");
+} else {
+    console.log("Reboot");
+}
